bfb5a097009435b4055957d65f899b6d82f04fa7
adkrueger <adkrueger@wpi.edu>
2020-03-17 22:31:41 -0400

    initial commit

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar-expand-lg navbar-light bg-light"></nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
