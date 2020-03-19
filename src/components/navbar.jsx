8ca6ac725ef05eb12f067743d8f3d3fe35e8e9db
adkrueger <adkrueger@wpi.edu>
2020-03-19 16:02:56 -0400

    added navbar.jsx

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#index">
+        Navigation{" "}
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
